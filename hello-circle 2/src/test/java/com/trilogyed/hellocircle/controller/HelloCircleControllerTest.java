2e013f06785310a4413ab48a9f85cfe1d327f7dc
Ephrem Binchebo <ephremgensa1@gmail.com>
2022-07-29 06:13:41 -0400

    file

--- hello-circle 2/src/test/java/com/trilogyed/hellocircle/controller/HelloCircleControllerTest.java
+++ hello-circle 2/src/test/java/com/trilogyed/hellocircle/controller/HelloCircleControllerTest.java
@@ -3,21 +3,13 @@ package com.trilogyed.hellocircle.controller;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import static org.junit.Assert.*;
 @WebMvcTest(HelloCircleController.class)
 @RunWith(SpringRunner.class)
 public class HelloCircleControllerTest {
